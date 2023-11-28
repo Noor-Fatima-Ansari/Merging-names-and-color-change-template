17115160e00cf4a232d4b98021ef00a409687cfc
ADMIN <noorfatima1233444@gmail.com>
2023-11-28 18:55:19 +0500

    Final Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Layout
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet.Constraint
+
+class MainActivity : AppCompatActivity() {
+
+
+//    var meargeData= "$firstValue $secondValue"
+  //  var secondValue
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_main)
+
+
+
+
+/*
+         **************MERGING THE FIELDS NAMES******************
+
+        var firstValue=findViewById<EditText>(R.id.entertext1)
+        var secondValue=findViewById<EditText>(R.id.entertext2)
+        var b1=findViewById<Button>(R.id.b1)
+        var h1=findViewById<TextView>(R.id.h1)
+       b1.setOnClickListener {
+           var first=firstValue.getText()
+           var second=secondValue.text
+           var merge= "$first  $second"
+           h1.setText(merge)
+       }
+
+*/
+
+
+
+/*
+      //********************** CHANGE COLOR *****************
+
+        var b1=findViewById<Button>(R.id.b1)
+        var b2=findViewById<Button>(R.id.b2)
+        var bg=findViewById<ConstraintLayout>(R.id.layout)
+
+      b1.setOnClickListener{
+          bg.setBackgroundColor(R.color.black)
+      }
+      b2.setOnClickListener{
+          bg.setBackgroundColor(R.color.white)
+      }
+*/
+ */
+
+
+
+
+
+
+
+    }
+}
